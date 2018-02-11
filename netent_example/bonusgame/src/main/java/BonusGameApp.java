4b4e7aa7d84121da02edda6945d1ac6f96181aff
Anna812 <88menyus@gmail.com>
2018-02-11 21:38:19 +0100

    create runnable App classes for SlotGame and BonusGame respectively

---
+++ netent_example/bonusgame/src/main/java/BonusGameApp.java
@@ -0,0 +1,10 @@
+package main.java;
+
+/**
+ * Created by Anna on 18/02/11.
+ */
+public class BonusGameApp {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ netent_example/slotgame/src/main/java/SlotGameApp.java
@@ -0,0 +1,10 @@
+package main.java;
+
+/**
+ * Created by Anna on 18/02/11.
+ */
+public class SlotGameApp {
+    public static void main(String[] args) {
+
+    }
+}
