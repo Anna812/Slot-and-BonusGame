0d445688984bf7b5e7030af9215ec7f0579a67dd
Anna812 <88menyus@gmail.com>
2018-02-11 21:43:03 +0100

    add empty constructor for Randomizer object

--- netent_example/framework/src/main/java/Randomizer.java
+++ netent_example/framework/src/main/java/Randomizer.java
@@ -1,7 +1,16 @@
 package main.java;
 
+import java.util.Random;
+
 /**
  * Created by Anna on 18/02/11.
  */
 public class Randomizer {
+    private Random random;
+
+    public Randomizer() {
+        random = new Random();
+    }
+
+    
 }
