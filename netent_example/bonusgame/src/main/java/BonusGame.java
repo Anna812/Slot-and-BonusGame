7b9c30c2edf06a5b2b171c994976354c5144d59c
Anna812 <88menyus@gmail.com>
2018-02-13 11:48:29 +0100

    add game logic to bonus game, refactor roll method in Randomiser

--- netent_example/bonusgame/src/main/java/BonusGame.java
+++ netent_example/bonusgame/src/main/java/BonusGame.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * Created by Anna on 18/02/12.
  */
@@ -8,6 +12,21 @@ public class BonusGame extends Game {
     }
 
     public void play() {
+        stats.bet += 10;
+        if(randomiser.ifRandomIsIn10Percent()) {
+            startBoxGame();
+        }
+    }
 
+    private void startBoxGame() {
+        ArrayList<Integer> boxes = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
+        Collections.shuffle(boxes);
+        int numberOfLoserBox = randomiser.roll(5) + 1;
+
+        for(int i = 1; i < 5; i++ ) {
+            if(boxes.get(i) != numberOfLoserBox) {
+                stats.win += 5;
+            }
+        }
     }
 }
--- netent_example/framework/src/main/java/Randomiser.java
+++ netent_example/framework/src/main/java/Randomiser.java
@@ -10,17 +10,17 @@ public class Randomiser {
         random = new Random();
     }
 
-    public int roll(){
-        return random.nextInt(10);
+    public int roll(int i){
+        return random.nextInt(i);
     }
 
     public boolean ifRandomIsIn30Percent() {
-        int rolledNumber = roll();
+        int rolledNumber = roll(10);
         return rolledNumber == 0 || rolledNumber == 1 || rolledNumber == 2;
     }
 
     public boolean ifRandomIsIn10Percent() {
-        int rolledNumber = roll();
+        int rolledNumber = roll(10);
         return rolledNumber == 0;
     }
 }
